638c9420653378a0f48f32c6f57ab6dc6be3a31a
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-05-02 18:32:20 +0200

    data logged

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [ items, setItems ] = useState([])
+  const [ reqType, setReqType ] = useState('users')
+
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch(`${API_URL}${reqType}`)
+      const data = await response.json()
+      console.log(data)
+    }
+    fetchItems()
+  }, [reqType])
+
+
   return (
-    <div className="App">
+  <div className="App">Hello!
     </div>
   );
 }
